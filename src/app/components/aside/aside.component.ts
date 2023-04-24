a139c8d1632abe9cf92960ebb1abb6436b3df073
Necdet UYGUR <necdet.uygur@gmail.com>
2023-04-24 20:10:27 +0300

    Aside

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { KullaniciService } from 'src/app/services/kullanici.service';
 
 @Component({
@@ -8,8 +8,16 @@ import { KullaniciService } from 'src/app/services/kullanici.service';
   styleUrls: ['./aside.component.css'],
 })
 export class AsideComponent {
+  CurrentRoute: string;
   constructor(
     public kullaniciService: KullaniciService,
     public router: Router
-  ) {}
+  ) {
+    this.CurrentRoute = '';
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        this.CurrentRoute = event.url;
+      }
+    });
+  }
 }
